17f562886a75c1e3873c2b15791c344ccb480948
Garmode3073 <gauravgarmode16@gmail.com>
2021-06-06 18:33:09 +0530

    adding globals and db services

---
+++ lib/globals.dart
@@ -0,0 +1,11 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+
+Color puple = Color(0xff571e73);
+Color purple2 = Color(0xff470e63);
+Color green = Color(0xff0ed145);
+Color deadGrey = Color(0xff292929);
+Color deadGrey2 = Color(0xff191919);
+
+StreamController<bool> controller = StreamController<bool>();
+Stream stream = controller.stream;
