bc5e46563241ea791eecd9e276304d0733fac70d
Garmode3073 <gauravgarmode16@gmail.com>
2021-06-07 22:06:17 +0530

    global changes

--- lib/globals.dart
+++ lib/globals.dart
@@ -1,11 +1,15 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 
-Color puple = Color(0xff571e73);
+Color purple = Color(0xff571e73);
 Color purple2 = Color(0xff470e63);
 Color green = Color(0xff0ed145);
+Color green2 = Color(0xff00a014);
 Color deadGrey = Color(0xff292929);
 Color deadGrey2 = Color(0xff191919);
+
+double width;
+double height;
 
 StreamController<bool> controller = StreamController<bool>();
 Stream stream = controller.stream;
