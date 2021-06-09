1eaf5e9446926ec2bb5cc9be5b268ac1d029b860
Garmode3073 <gauravgarmode16@gmail.com>
2021-06-09 23:22:34 +0530

    structure of register

--- lib/auth/register.dart
+++ lib/auth/register.dart
@@ -1,4 +1,7 @@
+import 'package:finalorder/common/textfields.dart';
 import 'package:flutter/material.dart';
+import 'package:finalorder/globals.dart' as g;
+import 'package:flutter/services.dart';
 
 class RegisterPage extends StatefulWidget {
   @override
@@ -6,8 +9,51 @@ class RegisterPage extends StatefulWidget {
 }
 
 class _RegisterPageState extends State<RegisterPage> {
+  final _regKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return WillPopScope(
+      onWillPop: () {
+        SystemNavigator.pop();
+        return Future.value(false);
+      },
+      child: GestureDetector(
+        onTap: () {
+          FocusScopeNode currentFocus = FocusScope.of(context);
+          if (!currentFocus.hasPrimaryFocus) {
+            currentFocus.unfocus();
+          }
+        },
+        child: Scaffold(
+          backgroundColor: g.purple2,
+          body: Form(
+            key: _regKey,
+            child: ListView(
+              children: [
+                TextFld(
+                  hint: 'Name',
+                ),
+                TextFld(
+                  hint: 'Email',
+                ),
+                NumFld(
+                  hint: 'Phone Number',
+                ),
+                NumFld(
+                  hint: 'Age',
+                ),
+                PassFld(
+                  hint: 'Password',
+                ),
+                PassFld(
+                  hint: 'Repeat Password',
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
